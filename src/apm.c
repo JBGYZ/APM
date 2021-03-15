ec228dd7ab114405e2e64b3ab13d863b5b2d6ace
Guanyu Zhang <guanyu.zhang@polytechnique.edu>
2021-03-15 17:37:24 +0100

    Add testing

--- src/apm.c
+++ src/apm.c
@@ -261,7 +261,7 @@ main( int argc, char ** argv )
                   (size_pattern+1) * sizeof( int ) ) ;
           return 1 ;
         }
-        gettimeofday(&t3, NULL);
+        // gettimeofday(&t3, NULL);
         #pragma omp parallel 
         {
         /* Traverse the input data up to the end of the file */
@@ -298,12 +298,12 @@ main( int argc, char ** argv )
         }
       }
 
-      gettimeofday(&t4, NULL);
-      duration = (t4.tv_sec -t3.tv_sec)+((t4.tv_usec-t3.tv_usec)/1e6);
+      //gettimeofday(&t4, NULL);
+      //duration = (t4.tv_sec -t3.tv_sec)+((t4.tv_usec-t3.tv_usec)/1e6);
       // printf( "OpenMP part done in %lf s, calculated by rank %d \n", duration) ;
 
       free( column );
-      MPI_Send(&duration, 1, MPI_INT, 0, rankMPI-1 + (sizeMPI-1)*j, MPI_COMM_WORLD);
+      MPI_Send(&matches_tmp, 1, MPI_INT, 0, rankMPI-1 + (sizeMPI-1)*j, MPI_COMM_WORLD);
 
       }
       
