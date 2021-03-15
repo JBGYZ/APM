659d706e0b2eb0210d926ae46ccb48fa1ead4249
Guanyu Zhang <guanyu.zhang@polytechnique.edu>
2021-03-15 16:19:29 +0100

    Add testing

--- src/apm.c
+++ src/apm.c
@@ -208,10 +208,12 @@ main( int argc, char ** argv )
   {
       size_pattern = strlen(pattern[i]) ;
       int ** columns ;
+      int * column;
       columns = (int **)malloc( omp_get_num_threads() * sizeof( int * ) ) ;
       for(j = 0 ; j < omp_get_num_threads() ; j++){
           columns[j] = (int *)malloc( (size_pattern+1) * sizeof( int ) ) ;
       }
+      column = (int *)malloc( (size_pattern+1) * sizeof( int ) ) ;
       /* Initialize the number of matches to 0 */
       n_matches[i] = 0 ;
 
@@ -239,7 +241,7 @@ main( int argc, char ** argv )
           {
               size = n_bytes - j ;
           }
-          distance = levenshtein( pattern[i], &buf[j], size, columns[omp_get_thread_num()] ) ;
+          distance = levenshtein( pattern[i], &buf[j], size, column ) ;
 
           if ( distance <= approx_factor ) {
               matches_tmp = matches_tmp + 1 ;
@@ -252,6 +254,7 @@ main( int argc, char ** argv )
         free(columns[j]);
       }
       free( columns );
+      free(column);
   }
   
 
